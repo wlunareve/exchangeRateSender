1eeead197bd7d61bdac1f0b6d684341608424982
wlunareve <wlunareve@gmail.com>
2018-11-21 14:27:38 +0800

    Fix bug

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ def scheduled_job():
     print('This job is run every day at UTC+8 8am.')
     os.system("python currencyCrawler.py")
 
-@sched.scheduled_job('interval', minute = 20)
+@sched.scheduled_job('cron', minute = 20)
 def timed_job_awake_your_app():
     print('awake app every 20 minutes.')
     url = 'https://currencycrawlersender.herokuapp.com/'
