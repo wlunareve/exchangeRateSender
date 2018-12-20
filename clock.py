49b9919f5d1a641b803d8586ff5172c89fc0ad69
wlunareve <wlunareve@gmail.com>
2018-12-20 21:43:54 +0800

    Stop print requests response

--- clock.py
+++ clock.py
@@ -19,6 +19,5 @@ def timed_job_awake_your_app():
     print('awake app every 20 minutes.')
     url = 'https://currencycrawlersender.herokuapp.com/'
     r = requests.get(url)
-    print(r.content)
     
 sched.start()
